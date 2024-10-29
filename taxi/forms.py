097a401f31f0bf700db027ba5b4dcd0e5f2dbea8
Roman Sokolov <gnonasis@gmail.com>
2024-10-29 11:57:39 +0200

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django import forms
-from .models import Driver, Car
+from taxi.models import Driver, Car
 
 
 class DriverCreationForm(UserCreationForm):
